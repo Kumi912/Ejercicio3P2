cb9637bbb6b3d6ac3abae03bb916772dd471974d
Kumi912 <flor.maribel999@gmail.com>
2025-02-13 20:06:13 -0600

    Cambio realizado

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ingrese la cantiad de agua potable que esta en su recibo"); //
+        double agua = double.Parse(Console.ReadLine());
+
+        Agua consumo = new Agua(agua);
+
+        Console.WriteLine("La cantidad de agua que consume su familia al mes es de " + consumo.CalcularConsumo() + " litros");
+    }
+
+    class Agua
+    {
+        public double Agua1 { get; set; }
+
+        public Agua(double agua)
+        {
+            Agua1 = agua;
+        }
+
+        public double CalcularConsumo()
+        {
+            return (Agua1 * 1000) / 2;
+        }
+    }
+}
